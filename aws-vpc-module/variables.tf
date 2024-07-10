565df30b4ecd0acfc6ce5988474b1e1f87f693c9
puneeth tiruveedula <tiruveedulapuneeth3102@gmail.com>
2024-07-10 22:04:07 +0530

    terraform

--- aws-vpc-module/variables.tf
+++ aws-vpc-module/variables.tf
@@ -1,3 +1,11 @@
+
+/*===== The VPC ======*/
 variable "vpc_cidr" {
-      
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+  default = true
 }

83722a42978fac3f2afe05a89bb24f21ae4563ea
puneeth tiruveedula <tiruveedulapuneeth3102@gmail.com>
2024-07-11 12:50:18 +0530

    terraform

--- aws-vpc-module/outputs.tf
+++ aws-vpc-module/outputs.tf
@@ -14,3 +14,14 @@ output "database_subnet_ids" {
   value = aws_subnet.database_subnet[*].id
 }
 
+output "database_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
+output "ig_id" {
+  value = aws_internet_gateway.ig.id
+}
--- aws-vpc-module/vpc.tf
+++ aws-vpc-module/vpc.tf
@@ -34,6 +34,8 @@ resource "aws_internet_gateway" "ig" {
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
   depends_on = [ aws_internet_gateway.ig ]
+   # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
 }
 
 /*** NAT Gateway for the private subnets ***/
@@ -97,4 +99,92 @@ resource "aws_subnet" "database_subnet" {
       Name = "${local.resource_name}-database-${local.az_names[count.index]}"
     }
   )
+}
+
+/*** Database subnet Group ***/
+resource "aws_db_subnet_group" "default" {
+  name = "${local.resource_name}"
+  subnet_ids = aws_subnet.database_subnet[*].id
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_group_tags,
+    {
+      Name ="${local.resource_name}"
+    }
+  )
+}
+
+/*** Routing table for public subnet ***/
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags = merge(
+    var.common_tags,
+    var.public_route_table_tags,
+    {
+      Name ="${local.resource_name}-public"   # expense-dev-public
+    }
+  )
+}
+
+/*** Routing table for private subnet ***/
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  tags = merge(
+    var.common_tags,
+    var.private_route_table_tags,
+    {
+      Name ="${local.resource_name}-private"   
+    }
+  )
+}
+
+/*** Routing table for database subnet ***/
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.vpc.id
+  tags = merge(
+    var.common_tags,
+    var.database_route_table_tags,
+    {
+      Name ="${local.resource_name}-database"   
+    }
+  )
+}
+
+/*** routes ***/
+resource "aws_route" "public_internet_gateway" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.ig.id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route" "database_nat_gateway" {
+  route_table_id = aws_route_table.databse.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.nat.id
+}
+
+/*** Route table and Subnet associations ***/
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = element(aws_subnet.public_subnet[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id = element(aws_subnet.private_subnet[*].id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id = element(aws_subnet.databse_subnet[*].id, count.index)
+  route_table_id = aws_route_table.database.id
 }

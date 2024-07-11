310fbea4f90774bd1e86d588220648051f605d47
puneeth tiruveedula <tiruveedulapuneeth3102@gmail.com>
2024-07-11 10:29:22 +0530

    terraform

--- aws-vpc-module/variables.tf
+++ aws-vpc-module/variables.tf
@@ -32,3 +32,91 @@ variable "igw_tags" {
   type = map
   default = {}
 }
+
+/*===== NAT ======*/
+variable "nat_gateway_tags" {
+    type = map 
+    default = {}
+}
+
+/*===== public subnet ======*/
+variable "public_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.public_subnet_cidr_tags) == 2
+    error_message = "Please provide 2 valid public subnet CIDR"
+  }
+}
+
+variable "public_subnet_cidrs_tags" {
+  type = map
+  default = {}
+}
+
+/*===== private subnet ======*/
+variable "private_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.private_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid private subnet CIDR"
+    }
+}
+
+variable "private_subnet_cidr_tags" {
+  type = map
+  default = {}
+}
+
+/*===== database subnet ======*/
+variable "database_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.database_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid database subnet CIDR"
+    }
+}
+
+variable "database_subnet_cidr_tags" {
+  type = map
+  default = {}
+}
+
+/*===== database subnet group ======*/
+variable "database_subnet_group_tags" {
+    type = map 
+    default = {}
+}
+
+/*===== public route table ======*/
+variable "public_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+/*===== private route table ======*/
+variable "private_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+/*===== database route table ======*/
+variable "database_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+/*===== peering ======*/
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+
+variable "acceptor_vpc_id" {
+  type = string
+  default = ""
+}
+
+variable "vpc_peering_tags" {
+  type = map 
+  default = {}
+}
--- aws-vpc-module/vpc.tf
+++ aws-vpc-module/vpc.tf
@@ -14,8 +14,11 @@ resource "aws_vpc" "vpc" {
     )
 }
 
+
+/*===== Subnets ======*/
+
 /*** Internet gateway for the public subnet ***/
-resource "aws_internet_gateway" "igw" {
+resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.vpc.id
 
   tags = merge(
@@ -27,3 +30,71 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
+/*** Elastic IP for NAT Gateway ***/
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+  depends_on = [ aws_internet_gateway.ig ]
+}
+
+/*** NAT Gateway for the private subnets ***/
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public_subnet[0].id
+
+  tags = merge(
+    var.common_tags,
+    var.nat_gateway_tags,
+    {
+      Name = "${local.resource_name}"
+    }
+  )
+}
+
+/*** Public subnet ***/
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  count = length(var.public_subnet_cidrs)
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnet_cidrs_tags,
+    {
+      Name = "${local.resource_name}-public-${local.az_names[count.index]}"
+    }
+  )
+}
+
+/*** Private subnet ***/
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  count = length(var.private_subnet_cidrs)
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.private_subnet_cidr_tags,
+    {
+      Name = "${local.resource_name}-private-${local.az_names[count.index]}"
+    }
+  )
+}
+
+/*** Database subnet ***/
+resource "aws_subnet" "database_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  count = length(var.database_subnet_cidrs)
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnet_cidr_tags,
+    {
+      Name = "${local.resource_name}-database-${local.az_names[count.index]}"
+    }
+  )
+}
